cea91f34a258462495849ad5e96442b76d0fe0d9
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-09-27 10:31:52 +0700

    add update status voucher

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -187,4 +187,21 @@ module.exports = {
       res.redirect('/voucher');
     }
   },
+  actionStatus: async (req, res) => {
+    try {
+      const { id } = req.params;
+      let voucher = await Voucher.findOne({ _id: id });
+
+      let status = voucher.status === 'Yes' ? 'No' : 'Yes';
+
+      voucher = await Voucher.findOneAndUpdate({ _id: id }, { status });
+      req.flash('alertMessage', 'Voucher status has been updated');
+      req.flash('alertStatus', 'success');
+      res.redirect('/voucher');
+    } catch (error) {
+      req.flash('alertMessage', `${error.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/voucher');
+    }
+  },
 };
