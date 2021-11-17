e417f1c81d183f729199ec6789be1bf5267f9dfd
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-11-17 14:02:45 +0700

    add api for edit profile picture

--- app/player/router.js
+++ app/player/router.js
@@ -9,8 +9,11 @@ const {
   historyDetail,
   dashboard,
   profile,
+  editProfile,
 } = require('./controller');
 const { isLoginPlayer } = require('../middleware/auth');
+const multer = require('multer');
+const os = require('os');
 
 router.get('/homepage', landingPage);
 router.get('/:id/detail', detailPage);
@@ -20,5 +23,11 @@ router.get('/history', isLoginPlayer, history);
 router.get('/history/:id/detail', isLoginPlayer, historyDetail);
 router.get('/dashboard', isLoginPlayer, dashboard);
 router.get('/profile', isLoginPlayer, profile);
+router.put(
+  '/profile',
+  isLoginPlayer,
+  multer({ dest: os.tmpdir() }).single('image'),
+  editProfile
+);
 
 module.exports = router;
