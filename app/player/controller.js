8b31f1e0b503ce75faab683cd731e61788f330b3
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-11-18 12:44:51 +0700

    add payment as result to detailPage

--- app/player/controller.js
+++ app/player/controller.js
@@ -30,10 +30,12 @@ module.exports = {
         .populate('nominals')
         .populate('user', '_id name phoneNumber');
 
+      const payment = await Payment.find().populate('banks');
+
       if (!voucher) {
         return res.status(404).json({ message: 'Voucher not found' });
       }
-      res.status(200).json({ data: voucher });
+      res.status(200).json({ data: voucher, payment });
     } catch (error) {
       res
         .status(500)
